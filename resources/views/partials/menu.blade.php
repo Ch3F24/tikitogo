f6c89db41db0414b48721ea9ef1f76662cf3112a
Chef <zsolt.horvath@wst.hu>
2021-11-23 00:14:25 +0100

    alacarte table

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,13 +1,23 @@
 @if($d['menu'])
-    <h1 class="text-4xl font-bold text-tiki-celeste">Étel</h1>
     @if($d['menu']->foods->count())
+        <h1 class="text-4xl font-bold text-tiki-celeste">Étel</h1>
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-y-4 gap-x-4 my-4 menu">
             @each('partials.foods', $d['menu']->foods, 'food')
+        </div>
+    @else
+        <div class="my-8 lg:my-16 text-center">
+            <p class="text-4xl font-bold text-tiki-celeste">Erre a napra még nincs elérhető menünk</p>
+            <p class="text-4xl font-bold text-tiki-celeste">Kérjük gyere vissza késöbb.</p>
         </div>
     @endif
     @if($d['menu']->alacarte->count())
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-y-4 gap-x-4 my-4 alacarte hidden">
             @each('partials.foods', $d['menu']->alacarte, 'food')
+        </div>
+    @else
+        <div class="my-8 lg:my-16 text-center">
+            <p class="text-4xl font-bold text-tiki-celeste">Erre a napra még nincs elérhető menünk</p>
+            <p class="text-4xl font-bold text-tiki-celeste">Kérjük gyere vissza késöbb.</p>
         </div>
     @endif
 
