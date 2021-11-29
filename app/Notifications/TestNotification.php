09e2466eb2fa025178334e37d8b61904c37ee39d
Chef <zsolt.horvath@wst.hu>
2021-11-29 12:52:30 +0100

    test notification

--- app/Notifications/TestNotification.php
+++ app/Notifications/TestNotification.php
@@ -41,6 +41,7 @@ class TestNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
+                    ->from('zsolt.horvath@wst.hu','Tiki To Go #16546')
                     ->line('The introduction to the notification.')
                     ->action('Notification Action', url('/'))
                     ->line('Thank you for using our application!');
